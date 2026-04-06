1ef50172bd835f768c33f564f3cd0b14ddc10077
Ruby Nahal <46203493+Rubynahal@users.noreply.github.com>
2026-04-06 11:37:51 -0700

    Update nimbus to v0.2.1

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -1,28 +1,28 @@
 class Nimbus < Formula
   desc "CLI tool for Loop8 AWS access management"
   homepage "https://github.com/Loop8io/int-nimbus"
-  version "v0.1.8"
+  version "v0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.1.8/nimbus_darwin_arm64.tar.gz"
-      sha256 "6f92f97491c14af01e1ec25988ffec2c6cd507db6c632b32eecbbf396aea7830"
+      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.2.1/nimbus_darwin_arm64.tar.gz"
+      sha256 "0a344523179d21e540b67f82da12ef24264fbbf1eb89be34d03c2d06f9cbf044"
     end
     on_intel do
-      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.1.8/nimbus_darwin_amd64.tar.gz"
-      sha256 "120f8619391b19fe58dd1fb4f379110957c615afa48bee99631b69ff2488e90d"
+      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.2.1/nimbus_darwin_amd64.tar.gz"
+      sha256 "f5ac6ad91c57bd33764282e6742ee68e601992b2b7638e386fd1789accacfcff"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.1.8/nimbus_linux_arm64.tar.gz"
-      sha256 "ceac1882f8ede6c0fc63d6fa723d33c7ad318d06d9803959233f71fff9050234"
+      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.2.1/nimbus_linux_arm64.tar.gz"
+      sha256 "ace9be9a41f2944b3e939839935d1593b48e6a29983bfe9ef6c2bf58f1a27ed2"
     end
     on_intel do
-      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.1.8/nimbus_linux_amd64.tar.gz"
-      sha256 "aea6913fa9c33c3021e083692b692537df916cb4d7898b4dafcf57b95be0ba3d"
+      url "https://github.com/Loop8io/homebrew-tap/releases/download/v0.2.1/nimbus_linux_amd64.tar.gz"
+      sha256 "33e5e44c540a7e59e93fe7072593fc956489b40fee34893694493a18272e7732"
     end
   end
 
